3f328e599bd784ff0bbb6245e40c5773ee31ff1f
Nickyfrs <nickyfrs@gmail.com>
2022-08-15 14:38:18 +0100

    setup development enviroment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-ni$*jx9yb@6l6+c*$%e6$)vq7ib2%%(u!-^#swsfl*-^=06%@6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
